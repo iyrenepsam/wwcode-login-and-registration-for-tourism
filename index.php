5ad0076c20f25d0627978829d9c3b7c6f34664fc
iyrenepsam <irenepsam@gmail.com>
2021-01-12 13:35:12 +0530

    to php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login screen</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4">
+                <table class="table">
+                    <tr>
+                        <td>Username</td>
+                        <td><input type="text" class="form-control"></td>  
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-primary"> LOGIN</button></td>
+                    </tr>
+                </table>
+
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium laborum asperiores cumque, perferendis incidunt voluptatum reiciendis repudiandae ab facere sit dolor quas! Impedit, mollitia sequi asperiores nemo aspernatur soluta praesentium!</div>
+            <div class="col col-12 col-sm-4 col-md-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, earum? Eveniet, eos. Necessitatibus et rem, minus laboriosam repellat eveniet maiores consequatur, exercitationem, architecto saepe eos dolorum voluptatibus repellendus molestiae nisi!</div>
+        </div>
+
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eos corrupti hic veritatis explicabo expedita obcaecati et nisi laudantium nesciunt accusantium alias est debitis consequatur laboriosam eaque voluptatem exercitationem quisquam.</div>
+            <div class="col col-12 col-sm-4 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repellat nemo, quo incidunt molestias ullam earum ab libero? Sunt, dignissimos. Soluta sit praesentium in, quibusdam esse cum laudantium aspernatur aperiam.</div>
+            <div class="col col-12 col-sm-4 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta sint debitis praesentium nulla eveniet maiores accusamus veniam vitae! Doloremque aspernatur non repellat? Impedit repellat soluta maxime dolorem deleniti accusantium quod.</div>
+        </div>
+    </div>
+</body>
+</html>
