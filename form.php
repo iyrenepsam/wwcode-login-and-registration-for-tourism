82be3025ff9446965c2791ff99db95fe6adbd802
iyrenepsam <irenepsam@gmail.com>
2021-01-12 13:34:48 +0530

    changed to php

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col col-12  col-sm-4 col-md-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet neque quae molestiae voluptas reiciendis adipisci? Dolores alias possimus odit atque quasi necessitatibus amet doloremque dolore perspiciatis, aliquam sapiente cumque culpa!</div>
+            <div class="col col-12  col-sm-4 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas hic dolore, officia inventore fugiat repellat quas eveniet ex magnam aut tempore velit magni molestias totam asperiores natus illo minima veritatis.</div>
+            <div class="col col-12 col-sm-4 col-md-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia sint quibusdam quis. Maxime cupiditate rerum, voluptatem quibusdam hic architecto laboriosam soluta ipsum dolorum porro necessitatibus aliquid nihil. Est, accusamus culpa?</div>
+        </div>
+    </div>
+</body>
+</html>
